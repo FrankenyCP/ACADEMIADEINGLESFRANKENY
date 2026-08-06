501fe3e0a1c697c226e2a5957ab6504475b8254d
whikc <whikc@DESKTOP-8G3ENJ2>
2026-08-06 01:37:41 -0400

    Agrega interfaz de servicio de correo

---
+++ CAPA_NEGOCIOS/IServicioCorreo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CAPA_NEGOCIOS
+{
+    public interface IServicioCorreo
+    {
+        Task EnviarRegistroExitosoAsync(
+            string correoDestino,
+            string nombreAlumno
+        );
+
+        Task EnviarConfirmacionPagoAsync(
+            string correoDestino,
+            string nombreAlumno,
+            decimal monto,
+            DateTime fechaPago,
+            string metodoPago
+        );
+    }
+}
